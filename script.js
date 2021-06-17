66b60ee38c133ac106cfa6d3bd7508a1f0e39de7
Alexander <alexkobayashi128@gmail.com>
2021-06-16 21:00:30 -0700

    added function which asks a prompt for the grid size to be displayed

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-function createSquare(){
-    for(i = 0; i < 16; i++){
+function createSquare(gridSize){
+    if(gridSize>=100){
+        alert('Your value is too high!');
+        document.querySelector('#boxContainer').remove();
+        return;
+    };
+    let value = Math.pow(gridSize, 2)
+    for(i = 0; i < value; i++){
         let square = document.createElement('span');
         square.classList.add('square');
-        square.textContent = ('box');
         document.getElementById('boxContainer').appendChild(square);
     }   
 };
-
-createSquare();
-
+let gridSizePrompt = parseInt(prompt('How big do you want your width and height for the box to be?'));
+createSquare(gridSizePrompt);
